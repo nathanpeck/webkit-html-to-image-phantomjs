2e04c9df7ffd69eec76cafca8437bd315c76c161
Nathan Peck <nathan@storydesk.com>
2013-04-11 19:11:01 +0000

    Filling out the logic a bit more, fixing things.

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 var page = require('webpage').create(),
     server = require('webserver').create();
+    
+function error(response,errorMessage)
+{
+	response.statusCode = 200;
+  response.headers = {
+      'Cache': 'no-cache',
+      'Content-Type': 'text/plain;charset=utf-8'
+  };
+  response.write(errorMessage);
+  response.close();
+}
 
 service = server.listen(3000, function (request, response) {
-		if(request.method == 'POST')
+	if(request.method == 'POST')
+	{
+		if(request.headers['Content-Type']=='text/html')
 		{
-      console.log(JSON.stringify(request, null, 4));
-      response.statusCode = 200;
-	    response.headers = {
-	        'Cache': 'no-cache',
-	        'Content-Type': 'application/json;charset=utf-8'
-	    };
-	    response.write(JSON.stringify(request, null, 4));
-	  }
+		  console.log("HTML body received");			
+		  
+		  console.log("Request completed")
+		}
+		else
+		{
+			console.log("POST received but the Content-Type header was not correct");
+		  error(response,"Header value Content-Type should be set to text/html");			
+		}
+	}
+	else
+	{
+		console.log("Non-POST request received");
+	  error(response,"Usage: Send a POST body containing the HTML to render. Be sure to set the Content-Type header to text/html");
+	}
 });
--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xvfb-run --server-args="-screen 0, 800x600x24" phantomjs
+xvfb-run --server-args="-screen 0, 800x600x24" phantomjs --disk-cache=yes app.js
